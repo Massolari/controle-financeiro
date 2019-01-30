a23743c4cf462cf1dc92ccd49410c0b1e2918dcf
Douglas M <douglasmassolari@nasajon.com.br>
2019-01-30 07:12:29 -0200

    Arquivos da store global

---
+++ src/store/mutations.js
@@ -0,0 +1,14 @@
+export const PREV_YEAR = (state) => {
+  state.data.ano--
+  state.data.mes = 12
+}
+export const PREV_MONTH = (state) => {
+  state.data.mes--
+}
+export const NEXT_YEAR = (state) => {
+  state.data.ano++
+  state.data.mes = 1
+}
+export const NEXT_MONTH = (state) => {
+  state.data.mes++
+}
---
+++ src/store/state.js
@@ -0,0 +1,6 @@
+export default {
+  data: {
+    mes: (new Date()).getMonth() + 1,
+    ano: (new Date()).getFullYear()
+  }
+}
